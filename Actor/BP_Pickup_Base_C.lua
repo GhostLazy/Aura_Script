1ec335ce51682f6623e5fb55e325498c0d58e8d3
GhostLazy <2265041410@qq.com>
2026-03-14 16:33:51 +0800

    32-16
    拾取物音效

--- Actor/BP_Pickup_Base_C.lua
+++ Actor/BP_Pickup_Base_C.lua
@@ -10,7 +10,9 @@
 local M = UnLua.Class()
 
 function M:ReceiveBeginPlay()
+    UE.UGameplayStatics.PlaySoundAtLocation(self, self.SpawnSound, self:K2_GetActorLocation(), self:K2_GetActorRotation())
     self.PickupElevationTimeline:Play()
+    self.bHitGround = false
 end
 
 function M:ReceiveTick(DeltaSeconds)
@@ -21,11 +23,20 @@ end
 function M:OnPickupElevationTimelineUpdate(Elevation, Scale)
     self.CalculatedLocation = self.InitialLocation + UE.FVector(0, 0, Elevation * 100)
     self:SetActorScale3D(UE.FVector(Scale, Scale, Scale))
+
+    if (not self.bHitGround and Elevation < 0) then
+        self.bHitGround = true
+        UE.UGameplayStatics.PlaySoundAtLocation(self, self.GroundImpactSound, self:K2_GetActorLocation(), self:K2_GetActorRotation())
+    end
 end
 
 function M:OnPickupElevationTimelineFinished()
     self:StartSinusoidalMovement()
     self:StartRotation()
+end
+
+function M:ReceiveDestroyed()
+    UE.UGameplayStatics.PlaySoundAtLocation(self, self.ConsumedSound, self:K2_GetActorLocation(), self:K2_GetActorRotation())
 end
 
 return M
