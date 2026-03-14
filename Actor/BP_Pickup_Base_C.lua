0f739996bb4d2a4ba38a027778abb8c2b46769ab
GhostLazy <2265041410@qq.com>
2026-03-14 16:18:36 +0800

    32-15
    拾取物生成特效

--- Actor/BP_Pickup_Base_C.lua
+++ Actor/BP_Pickup_Base_C.lua
@@ -10,13 +10,22 @@
 local M = UnLua.Class()
 
 function M:ReceiveBeginPlay()
-    self:StartSinusoidalMovement()
-    self:StartRotation()
+    self.PickupElevationTimeline:Play()
 end
 
 function M:ReceiveTick(DeltaSeconds)
     self:K2_SetActorLocation(self.CalculatedLocation, false, nil, false)
     self:K2_SetActorRotation(self.CalculatedRotation, false)
+end
+
+function M:OnPickupElevationTimelineUpdate(Elevation, Scale)
+    self.CalculatedLocation = self.InitialLocation + UE.FVector(0, 0, Elevation * 100)
+    self:SetActorScale3D(UE.FVector(Scale, Scale, Scale))
+end
+
+function M:OnPickupElevationTimelineFinished()
+    self:StartSinusoidalMovement()
+    self:StartRotation()
 end
 
 return M
